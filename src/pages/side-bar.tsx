825a0eb114ecd652efe0caa0d04d27d93c4a9d38
shaline <shalinewambui04@gmail.com>
2025-03-04 12:53:20 +0300

    Added hovering effects on the links in the sidebar

--- src/pages/side-bar.tsx
+++ src/pages/side-bar.tsx
@@ -8,26 +8,26 @@ export const SideBar = () => {
       <h2 className="logo">JANE SMITH</h2>
       <ul>
         <li>
-          <Link to="/search">
+          <Link to="/search" className="sidebar-item">
             <FaUsers className="icon" />
             <span>Search Friends</span>
           </Link>
         </li>
 
         <li>
-          <Link to="/user-details">
+          <Link to="/user-details" className="sidebar-item">
             <FaComment className="icon" />
             <span>Chats</span>
           </Link>
         </li>
         <li>
-          <Link to="/user-details">
+          <Link to="/user-details" className="sidebar-item">
             <FaUser className="icon" />
             <span>Profile</span>
           </Link>
         </li>
         <li>
-          <Link to="/">
+          <Link to="/" className="sidebar-item">
             <FaSignOutAlt className="icon" />
             <span>Logout</span>
           </Link>
